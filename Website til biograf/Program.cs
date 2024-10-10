4cc44e7d639387840c087fd7b4fb91376440ce03
Loui510s <Louie.Ferdinand05@gmail.com>
2024-10-10 13:53:06 +0200

    Tilføjet sædevalg og log in side

--- Website til biograf/Program.cs
+++ Website til biograf/Program.cs
@@ -7,6 +7,10 @@ namespace Website_til_styring_af_biograf
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Indtast navn");
+            Console.ReadLine();
+            Console.WriteLine("Indtast kodeord");
+            Console.ReadLine();
             // Opret en liste af film
             List<string> filmListe = new List<string>
             {
@@ -14,9 +18,9 @@ namespace Website_til_styring_af_biograf
                 "Shrek 4",
                 "Fat Albert"
             };
-
+        FilmValg:
             // Udskriv menuen med filmene
-            Console.WriteLine("Vælg en film ved at indtaste nummeret:");
+            Console.WriteLine("Vælg en film du gerne vil se, ved at indtaste nummeret:");
             for (int i = 0; i < filmListe.Count; i++)
             {
                 Console.WriteLine($"{i + 1}. {filmListe[i]}");
@@ -35,10 +39,47 @@ namespace Website_til_styring_af_biograf
             }
             else
             {
+                //udspyt fejlkode og lad dem prøve igen
                 Console.WriteLine("Ugyldigt valg. Prøv igen.");
+                goto FilmValg;
+            }
+            SædeValg:
+            //opret en liste med sæderne
+            List<string> sædeListe = new List<string>
+                {
+                    "Sædet helt til venstre",
+                    "Sædet til venstre fra midten",
+                    "Sædet i midten",
+                    "Sædet til højre fra midten",
+                    "Sædet helt til højre"
+                };
+            // Udskriv menuen med sæderne
+            Console.WriteLine("Vælg et sæde ved at indtaste nummeret:");
+            for (int i = 0; i < sædeListe.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {sædeListe[i]}");
             }
 
+            // Læs brugerens input
+            Console.Write("Indtast filmnummer: ");
+            string input2 = Console.ReadLine();
+            int sædeValg;
+
+            // Kontroller om input er et gyldigt tal og inden for det rigtige område
+            if (int.TryParse(input2, out sædeValg) && sædeValg > 0 && sædeValg <= sædeListe.Count)
+            {
+                // Vis den valgte film
+                Console.WriteLine($"Du har valgt: {sædeListe[sædeValg - 1]}");
+            }
+            else
+            {
+                //udspyt fejlkode og lad dem prøve igen
+                Console.WriteLine("Ugyldigt valg. Prøv igen.");
+
+                goto SædeValg;
+            }
             Console.ReadLine();
         }
     }
 }
+
