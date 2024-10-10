5200d40d0a9c1ca1adf63f63cf3726fb18efa246
Loui510s <Louie.Ferdinand05@gmail.com>
2024-10-10 10:54:18 +0200

    Projektet er lagt ind, og vi kan se filmene der kører

---
+++ Website til biograf/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Website_til_styring_af_biograf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string film1 = "How To Train Your Dragon 2";
+            string film2 = "Shrek 4";
+            string film3 = "Fat Albet";
+            Console.WriteLine(film1);
+            Console.WriteLine(film2);
+            Console.WriteLine(film3);
+            Console.ReadLine();
+        }
+    }
+}
